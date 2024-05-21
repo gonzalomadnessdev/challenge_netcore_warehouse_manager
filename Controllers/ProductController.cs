4af192764f1e40346e24ba9e8912715053cbe437
Gonzalo Albornoz <gonzalo.hr.albornoz@gmail.com>
2024-05-20 23:45:19 -0300

    implemento dispatchProduct

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -84,5 +84,40 @@ namespace warehouse_manager.Controllers
 
             return new OkResult();
         }
+
+        [HttpPost("dispatch")]
+        public IActionResult DispatchProduct(int productId, int qty)
+        {
+            if (qty <= 0)
+            {
+                return new BadRequestObjectResult(new NotPositiveQuantityMessage());
+            }
+
+            IEnumerable<ProductRecord> products = _warehouseRepository.GetProductRecords(p => p.ProductId == productId);
+            ProductRecord product = products.FirstOrDefault()!;
+
+            if (product == null)
+            {
+                throw new Exception("Cannot receive. There is no product for given product ID.");
+            }
+
+            IEnumerable<CapacityRecord> capacities = _warehouseRepository.GetCapacityRecords(c => c.ProductId == productId);
+            CapacityRecord capacity = capacities.FirstOrDefault()!;
+
+            if (capacity == null)
+            {
+                throw new Exception("Cannot receive. There is no capacity for given product ID.");
+            }
+
+            int currentQty = product.Quantity - qty;
+
+            if (currentQty < 0)
+            {
+                return new BadRequestObjectResult(new QuantityTooHighMessage());
+            }
+            _warehouseRepository.SetProductRecord(productId, currentQty);
+
+            return new OkResult();
+        }
     }
 }
